9580929bdf576478bb02f15539a88326fed54730
Jonas Palzer <j.palzer@queo.onmicrosoft.com>
2023-12-12 14:17:56 +0100

    start day5

---
+++ Day5/Day5.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode23;
+
+public partial class Day5
+{
+    readonly string[] input = FileReader.ReadFile(@"\Day5\Test.txt");
+
+    public void Part1()
+    {
+        string[] initialSeeds = InitalSeeds().Matches(input[0]).Select(m => m.ToString()).ToArray();    
+
+    }
+
+    [GeneratedRegex(@"(\d)+")]
+    private static partial Regex InitalSeeds();
+}
