a59233b1fe34e226df8a35a02e231eaf3ca5fed4
Jonas Palzer <j.palzer@queo.onmicrosoft.com>
2023-12-13 16:16:50 +0100

    thx for free Nikolaus stars

--- Day6/Day6.cs
+++ Day6/Day6.cs
@@ -1,19 +1,37 @@
 ﻿namespace AdventOfCode23;
 
 public class Day6
-{
-    //readonly List<(int time, int distance)> Puzzle = [(7, 9), (15, 40), (30, 200)];
-
-    readonly List<(int time, int distance)> Puzzle = [(40, 215), (92,1064),(97,1505),(90,1100)];
-
+{   
     public void Part1()
     {
+        //List<(int time, int distance)> Puzzle = [(7, 9), (15, 40), (30, 200)];
+        List<(int time, int distance)> Puzzle = [(40, 215), (92,1064),(97,1505),(90,1100)];
+
         int result = 1;
         foreach (var (time, distance) in Puzzle)
         {
             result *= TotalWaysToWin(time, distance);
         }
         Console.WriteLine("\nResult: " + result);
+    }
+
+    public void Part2()
+    {
+        TotalWays2(40929790, 215106415051100);
+    }
+
+    private long TotalWays2(int time, long distance)
+    {
+        long wins = 0;
+        for (long speed = 1; speed < time; speed++)
+        {
+            long remainingTimeToTravel = time - speed;
+            long drivenDisance = remainingTimeToTravel * speed;
+            if (drivenDisance > distance) wins++;
+        }
+
+        Console.WriteLine(wins);
+        return wins;
     }
 
     private int TotalWaysToWin(int time, int distance)
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode23;
 
 var day = new Day6();
-day.Part1();
+day.Part2();
